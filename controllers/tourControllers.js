7c1056ae02cc43e38ab338ee48755f7a368c7619
Karan-Kumar-Pandit <karanbgs2000@gmail.com>
2023-06-25 14:30:00 +0530

    Initial commit

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,76 +1,24 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 //  Rout Handlers
-// ---------------------------------------------
-// exports.getAllTours = async (req, res) => {
-//   try {
-//     // BUILD QUERY
-//     // 1A) Filtering
-//     const queryObj = { ...this.queryString };
-//     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-//     excludedFields.forEach((el) => delete queryObj[el]);
-
-//     // 1B) Advanced filtering
-//     let queryStr = JSON.stringify(queryObj);
-//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-//     // console.log(JSON.parse(queryStr));
-
-//     let query = Tour.find(JSON.parse(queryStr));
-
-//     // 2) Sorting
-//     if (req.query.short) {
-//       const sortBy = req.query.sort.split(',').json(' ');
-//       console.log(sortBy);
-//       query = query.sort(sortBy);
-//       // sort('price ratingsAverage')
-//     }
-
-//     // const tours = await Tour.find()
-//     //   .where('duration')
-//     //   .equals(5)
-//     //   .where('difficulty')
-//     //   .equals('easy');
-
-//     // EXECUTE QUERY
-//     const tours = await query;
-
-//     // SEND RESPONSE
-//     res.status(200).json({
-//       status: 'success',
-//       results: tours.length,
-//       data: {
-//         tours,
-//       },
-//     });
-//   } catch (err) {
-//     res.status(404).json({
-//       status: 'fail',
-//       massage: err,
-//     });
-//   }
-// };
-
+// --------------------------------------------
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    const query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query.sort(sortBy);
-    }
-
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
@@ -81,6 +29,7 @@ exports.getAllTours = async (req, res) => {
       },
     });
   } catch (err) {
+    // console.error(err); // Log the error message
     res.status(404).json({
       status: 'fail',
       message: err,
--- models/tourModel.js
+++ models/tourModel.js
@@ -64,7 +64,7 @@ const tourSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now(),
-    // select: false
+    select: false,
   },
   startDates: [Date],
   // secretTour: {
